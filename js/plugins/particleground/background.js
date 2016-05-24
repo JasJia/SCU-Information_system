680df90203bccdb17d495bd1ff32ebed22c09278
Jasper Jia <jiashizhen@caacetc.com>
2016-05-24 14:51:34 +0800

    Revert "new item"
    This reverts commit 74bccf8a2b446da5b9efad63ec39a24f8b8ca65f.

--- js/plugins/particleground/background.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#517DCA',
-    lineColor: '#517DCA'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
